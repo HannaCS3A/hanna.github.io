1efd4117d692568fcd9a810736791f8d1b405c04
Ramelle Mhiro F. Fortuna <rafortuna@my.cspc.edu.ph>
2025-05-22 22:16:40 +0800

    Added Ceasar Cipher Algorithm

---
+++ algorithms/Ceasar_Cipher.py
@@ -0,0 +1,12 @@
+def caesar_encrypt(text, shift):
+    encrypted = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            encrypted += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            encrypted += char
+    return encrypted
+
+def caesar_decrypt(ciphertext, shift):
+    return caesar_encrypt(ciphertext, -shift)
